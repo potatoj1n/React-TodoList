ec56b5066c8d101572bc4fc83a902cf3eb357a17
potatoj1n <appabomul@gmail.com>
2024-04-01 17:24:31 +0900

    체크된 항목 따로 정렬

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,23 @@ import useTodos from 'pnpm-react/src/hooks/useTodos.tsx';
 import TodoItem from './TodoItem';
 
 function TodoList() {
-  const todos = useTodos();
+  const { todos, sortUncheckedTodos } = useTodos();
+
+  const checkedTodos = todos.filter(todo => todo.done);
+  const uncheckedTodos = sortUncheckedTodos(todos);
   return (
-    <ul className="max-h-9flex-wrap relative flex-col overflow-y-auto" style={{ maxHeight: '400px' }}>
-      {todos.map(todo => (
-        <TodoItem todo={todo} key={todo.id} />
-      ))}
-    </ul>
+    <div className="relative max-h-96 overflow-y-auto" style={{ maxHeight: '400px' }}>
+      <ul className="flex-col">
+        {uncheckedTodos.map(todo => (
+          <TodoItem todo={todo} key={todo.id} />
+        ))}
+        <div className="mt-auto">
+          {checkedTodos.map(todo => (
+            <TodoItem todo={todo} key={todo.id} />
+          ))}
+        </div>
+      </ul>
+    </div>
   );
 }
 
--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -1,10 +1,14 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/reducers';
+import { Todo } from '../store/reducers/Todo';
 
 export default function useTodos() {
   const todos = useSelector((state: RootState) => state.todo);
-  const sortedTodos = [...todos].sort((a, b) => {
-    return new Date(b.todoDate).valueOf() - new Date(a.todoDate).valueOf();
-  });
-  return sortedTodos;
+  const sortUncheckedTodos = (todos: Todo[]) => {
+    return todos
+      .filter(todo => !todo.done)
+      .sort((a, b) => new Date(b.todoDate).valueOf() - new Date(a.todoDate).valueOf());
+  };
+
+  return { todos, sortUncheckedTodos };
 }
