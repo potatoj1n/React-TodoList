2c6a7e6838c06d68ee6c92c6cea321b49fec639d
potatoj1n <appabomul@gmail.com>
2024-04-05 18:34:25 +0900

    redux-persist 스토어 수정

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import rootReducer from './reducers/index.ts';
+
+import { combineReducers } from 'redux';
+import todo, { TodosState } from './reducers/Todo';
+
+const rootReducer = combineReducers({
+  todo,
+});
 
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['todos'],
 };
+
+export interface RootState {
+  todo: TodosState;
+}
 
 const reducer = persistReducer(persistConfig, rootReducer);
 
